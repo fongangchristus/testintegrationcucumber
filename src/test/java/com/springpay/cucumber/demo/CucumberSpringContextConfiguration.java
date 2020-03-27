68248fb81806e9f8af9938d81b0ebdd347e8b5c0
christus <christus.tchassi@yahoo.fr>
2020-03-27 11:07:28 +0100

    Update CucumberSpringContextConfiguration.java

--- src/test/java/com/springpay/cucumber/demo/CucumberSpringContextConfiguration.java
+++ src/test/java/com/springpay/cucumber/demo/CucumberSpringContextConfiguration.java
@@ -7,22 +7,28 @@ import cucumber.api.java.en.Given;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertTrue;
 
 /**
  * Class to use spring application context while running cucumber
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@ContextConfiguration(classes = DemoApplication.class, loader = SpringBootContextLoader.class)
-public abstract class CucumberSpringContextConfiguration {
+@ContextConfiguration(classes = DemoApplication.class, loader = SpringBootContextLoader.class)
+public class CucumberSpringContextConfiguration {
     private static final Logger log = LoggerFactory.getLogger(CucumberSpringContextConfiguration.class);
+    @Before
+    public void setup_cucumber_spring_context(){
+        // Dummy method so cucumber will recognize this class as glue
+        // and use its context configuration.
+        //  log.info("--------------------------------------------------------------");
+        log.info("--------Application context is loading for test---------------");
+        // log.info("--------------------------------------------------------------");
+
+    }
 }
 
